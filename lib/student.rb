d377a519f7ccb7544ff05626fee2043d437e9988
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-06-22 19:24:57 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -70,7 +70,7 @@ class Student
     sql = <<-SQL
       SELECT *
       FROM students
-      WHERE students.grade = "9"
+      WHERE students.grade = 9
     SQL
     
     DB[:conn].execute(sql).map do |row|

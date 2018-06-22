a27e7d8174d03dead3471fad099498a8b8d13730
Hector Ariceaga <hector_ariceaga@hotmail.com>
2018-06-22 19:21:47 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -66,4 +66,9 @@ class Student
     DB[:conn].execute(sql)
   end
   
+  def self.count_all_students_in_grade_9
+    sql = <<-SQL
+      SELECT *
+      FROM students
+      WHERE students.grade = "9th"
 end
